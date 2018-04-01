7a9bd1ec776f6df662a54250a7576d2002443455
grantist <kubmet23@gmail.com>
2018-04-01 16:21:42 +0300

    HW2

--- lesson/lesson6.md
+++ lesson/lesson6.md
@@ -15,6 +15,6 @@
 
 ## Домашнее задание
 - Сделать рефакторинг тестов: `saveOverflow` должно быть только для Array реализаций.
-- Рефакторинг: в конструктор Resume добавить второй параметр `fullName`
-- Сделать рефакторинг всех реализаций `Storage`: заменить метод `Resume[] getAll()` на `List<Resume> getAllSorted()`
+- Рефакторинг: в конструктор com.javops.webapp.model.Resume добавить второй параметр `fullName`
+- Сделать рефакторинг всех реализаций `Storage`: заменить метод `com.javops.webapp.model.Resume[] getAll()` на `List<com.javops.webapp.model.Resume> getAllSorted()`
 - Реализовать до конца `MapUuidStorage`. Подумать что еще может быть search key в реализации на основе Map.
---
+++ src/com/javops/webapp/storage/ArrayStorage.java
@@ -0,0 +1,84 @@
+package com.javops.webapp.storage;
+
+import com.javops.webapp.model.Resume;
+
+import java.util.Arrays;
+
+/**
+ * Array based storage for Resumes
+ */
+public class ArrayStorage {
+    private Resume[] storage = new Resume[10000];
+    private int size = 0;//количество элементов в массиве
+
+    public void clear() {
+        Arrays.fill(storage, null);
+        size = 0;
+    }
+
+    public void update(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index == -1) {
+            System.out.println("Resume " + r.getUuid() + " not exist.");
+
+        } else {
+            storage[index] = r;
+        }
+    }
+    public void save(Resume r) {
+        if (getIndex(r.getUuid()) != -1) {
+            System.out.println("Resume " + r.getUuid() + " already exist.");
+        } else if (size == storage.length) {
+            System.out.println("Storage overflow.");
+        } else {
+            storage[size] = r;
+            size++;
+        }
+
+    }
+
+    public Resume get(String uuid) {
+        int index = getIndex(uuid);
+        if (getIndex(uuid) == -1) {
+            System.out.println("Resume " + uuid + " not exist.");
+            return null;
+        }
+        return storage[index];
+    }
+
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (getIndex(uuid) == -1) {
+            System.out.println("Resume " + uuid + " not exist.");
+        } else {
+            size--;
+            storage[index] = storage[size];
+            storage[size] = null;
+        }
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    public Resume[] getAll() {
+        Resume[] result = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            result[i] = storage[i];
+        }
+        return result;
+    }
+
+    public int size() {
+
+        return size;
+    }
+
+    private int getIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
